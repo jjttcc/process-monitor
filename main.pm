5c8aa2cc662e73e02efc0c6af72971c96561b1b8
Jim Cochrane <jim.cochrane@gmail.com>
2014-03-06 20:31:04 -0600

    Finished modularization, etc.
    Finished the work started with the last check-in by removing the class
    code (which is now essentially duplicated in ConstraintManager.pm and
    ProcessConstraint.pm) from main.pm - i.e., main.pm is now just the
    main-level logic.

--- main.pm
+++ main.pm
@@ -1,92 +1,10 @@
-#!/usr/bin/env perl
-# Monitor (and, optionally, act on) processes matching a configuration.
 
 use Modern::Perl;
 use constant::boolean;
 use Proc::ProcessTable;
 use Data::Dumper;
-
-
-# Objects that check for violations based on a particular constraint on a
-# process
-package ProcessConstraint;
-
-use Moose;
-use Data::Dumper;
-use constant::boolean;
-
-# memory-size limit
-has memlimit => (
-	is      => 'ro',
-	isa     => 'Int',
-	default => sub {-1},
-);
-
-# Reason last call to 'conforms' returned false
-has last_violation => (
-	is      => 'rw',
-	isa     => 'Str',
-	default => sub {''},
-);
-
-sub conforms {
-	my ($self, $proc) = @_;
-	my $result = TRUE;
-	if ($self->memlimit >= 0 and $proc->rss > $self->memlimit) {
-		$self->last_violation('memory/rss limit: ' .
-            $proc->rss / 1024 . ' > ' . $self->memlimit / 1024);
-		$result = FALSE;
-	}
-	$result;
-}
-
-
-# Manager of constraint-checking logic
-package ConstraintManager;
-
-use Moose;
-use Data::Dumper;
-use constant::boolean;
-
-has constraints => (
-    is  => 'rw',
-    isa => 'ArrayRef[ProcessConstraint]',
-);
-
-has patterns => (
-    is  => 'rw',
-    isa => 'ArrayRef[Str]',
-);
-
-has process_candidates => (
-    is  => 'rw',
-    isa => 'ArrayRef',
-);
-
-sub BUILD {
-    my ($self) = @_;
-    my $patterns = [];
-    if (@ARGV > 0) {
-        $patterns = \@ARGV;
-    } else {
-        $patterns = [qr/\bchrome\b/i, qr/\bvlc\b/];
-    }
-    $self->patterns($patterns);
-
-    my $proctable = Proc::ProcessTable->new();
-    my @candidates = grep {
-        my $match = FALSE;
-        for my $p (@$patterns) {
-            if ($_->cmndline =~ $p or $_->fname =~ $p) {
-                $match = TRUE;
-                last;
-            }
-        }
-        $match;
-    } @{$proctable->table};
-    $self->constraints([ProcessConstraint->new(memlimit => 111 * 1024 * 1024)]);
-    $self->process_candidates(\@candidates);
-}
+use ProcessConstraint;
+use ConstraintManager;
 
 package main;
 
